ae98ef87a6f6f7d8820043d8fea283ddb19434ee
Faraday <faradaytrs@gmail.com>
2014-10-20 20:56:28 +0400

    Implemented initial file structure.

--- src/Tanks.java
+++ src/Tanks.java
@@ -2,7 +2,11 @@
  * Created by Andrey Izotov on 20.10.2014.
  */
 public class Tanks {
+
 	public static void main(String[] args) {
 
+
+
 	}
+
 }
---
+++ src/engine/Engine.java
@@ -0,0 +1,10 @@
+package engine;
+
+/**
+ * Created by Andrey Izotov on 20.10.2014.
+ */
+public class Engine {
+	public Engine() {
+
+	}
+}
